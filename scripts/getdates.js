1e068801ab24e85a06a67457930238d396f70896
Osabuohien Ehigiator <randalfosabuohien@gmail.com>
2024-09-22 08:21:22 +0100

    Issue with the heading and content not having card as the parent class and thus audit tool not verifying the quick links added - I have a total of ten(10) links yet the audit tool says I have just 4

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,3 +2,6 @@ const lastModifiedDate = document.lastModified;
 document.getElementById(
   "lastModified"
 ).textContent = `Last Modification: ${lastModifiedDate}`;
+
+const currentYear = new Date().getFullYear();
+document.getElementById("currentYear").textContent = currentYear;
