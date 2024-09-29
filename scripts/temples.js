11f39a7ad056d1a20a2a873e81ea861ce058b5b2
guyrandalf <randalfosabuohien@gmail.com>
2024-09-29 21:52:15 +0100

    feat temple album with responsiveness after a mobile first approach

---
+++ scripts/temples.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const hamburger = document.querySelector('.hamburger');
+    const navMenu = document.querySelector('nav ul');
+  
+    hamburger.addEventListener('click', () => {
+      navMenu.classList.toggle('show');
+  
+      if (navMenu.classList.contains('show')) {
+        hamburger.innerHTML = '✖';
+      } else {
+        hamburger.innerHTML = '☰';
+      }
+    });
+  
+    const lastModifiedSpan = document.getElementById('last-modified');
+    lastModifiedSpan.textContent = document.lastModified;
+  });
